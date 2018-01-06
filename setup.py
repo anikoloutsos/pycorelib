c8449617e63d6be0db07256af008e40f2d4618d8
anikoloutsos <anikoloutsos@gmail.com>
2018-01-06 21:23:45 +0200

    Modified setup.py and requirements-base.txt to standardise the way the requirements are defined

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
-
 from setuptools import setup
+
+
+def get_requirements(env):
+    with open('requirements-{}.txt'.format(env)) as fp:
+        return [x.strip() for x in fp.read().split('\n') if not x.startswith('#')]
+
+
+install_requires = get_requirements('base')
 setup(
     version='0.1.0',
     name='pycorelib',
-    packages=['data', 'data.input', 'data.output', 'data.preprocessing', 'data.visualization', 'system', 'system.utils',
-              'infobackup'],
+    packages=['data', 'data.input', 'data.output', 'data.preprocessing', 'data.visualization', 'system',
+              'system.utils'],
     license='add license here',
     author='Alexandros Nikoloutsos',
     author_email='anikoloutsos@gmail.com',
     description='Add description here',
-    install_requires=[
-        'pandas',
-        'numpy'
-    ]
+    install_requires=install_requires
 )
